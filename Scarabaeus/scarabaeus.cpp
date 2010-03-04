03e99edaf853680cdb1b39b1f0858661dcf81d53
s.p.vandeven <s.p.vandeven@bb6c2877-1723-60e2-c9b9-db87233550d1>
2010-03-04 20:21:48 +0000

    Scarabaeus class correct geimplementeerd. De curve is alleen wel gedraait maar goed als dat alles is.
    git-svn-id: https://svn.kenai.com/svn/uu3dmod~source-code-repository@9 bb6c2877-1723-60e2-c9b9-db87233550d1

--- Scarabaeus/scarabaeus.cpp
+++ Scarabaeus/scarabaeus.cpp
@@ -1,4 +1,7 @@
 #include "scarabaeus.h"
+#include <math.h>
+
+#define PI 3.14159265
 
 Scarabaeus::Scarabaeus(double a, double sampleDensity)
     : a(a), sampleDensity(sampleDensity)
@@ -15,6 +18,25 @@ QVector<QPointF> Scarabaeus::generatePoints()
     QVector<QPointF> result;
 
     /// TODO: implement point generation!
+
+    // Formule
+    // r(theta)= cos(2theta) - (a cos(theta))
+    // domein van -phi t/m phi
+
+    double hyp, opp, adj;
+
+    for(double theta = -PI ;theta <= PI; (theta = theta + sampleDensity))
+    {
+
+        hyp = (cos (2 * theta)) - (a * cos (theta));
+
+        opp = sin (theta) * hyp;
+        adj = cos (theta) * hyp;
+
+        QPointF point( opp, adj);
+        result.push_back(point);
+
+    }
 
     return result;
 }
